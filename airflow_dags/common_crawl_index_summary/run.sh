5a73781167420225d7abd91ede748b03dc7d16c2
Aaron Son <aaron@liquidata.co>
2020-02-11 16:41:08 -0800

    airflow_dags/common_crawl_index_summary: run.sh: Try pulling and pushing from dolthub.

--- airflow_dags/common_crawl_index_summary/run.sh
+++ airflow_dags/common_crawl_index_summary/run.sh
@@ -6,15 +6,8 @@ cd $root
 ulimit -n 16384
 go run . "$workdir"/index_summary.psv
 cd "$workdir"
-dolt init
-dolt sql -q 'CREATE TABLE `entry_stats` (
-  `host` LONGTEXT NOT NULL COMMENT '\''tag:0'\'',
-  `prefix` LONGTEXT NOT NULL COMMENT '\''tag:1'\'',
-  `status` LONGTEXT NOT NULL COMMENT '\''tag:2'\'',
-  `mime_detected` LONGTEXT NOT NULL COMMENT '\''tag:3'\'',
-  `languages` LONGTEXT NOT NULL COMMENT '\''tag:4'\'',
-  `count` BIGINT COMMENT '\''tag:5'\'',
-  `size` BIGINT COMMENT '\''tag:6'\'',
-  PRIMARY KEY (`host`,`prefix`,`status`,`mime_detected`,`languages`)
-)'
+dolt clone Liquidata/common-crawl-index-stats
 dolt table import -r entry_stats index_summary.psv
+dolt add .
+dolt commit -m 'Importing new data.'
+dolt push
