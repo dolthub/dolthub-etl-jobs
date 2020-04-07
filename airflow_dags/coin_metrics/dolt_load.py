92a79412208f02fbaa6d48425b9da2e84905ff17
Oscar Batori <oscar@liquidata.co>
2020-04-07 08:52:21 -0700

    Fix broken import and remove random README.md

--- airflow_dags/coin_metrics/dolt_load.py
+++ airflow_dags/coin_metrics/dolt_load.py
@@ -1,7 +1,5 @@
-
-from doltpy.etl import get_df_table_writer, get_dolt_loader
 import requests
-from airflow_dags.coin_metrics.coinlist_config import COINLIST, get_data_url
+from coin_metrics.coinlist_config import COINLIST, get_data_url
 import pandas as pd
 from io import BytesIO
 import logging
