2f360a6bfd449fa1c379621aa882c92832cc0455
Andy Arthur <andy@liquidata.co>
2020-04-16 12:23:57 -0700

    copied files from common_crawl_index_summary

---
+++ airflow_dags/nvd_cve/go.mod
@@ -0,0 +1,5 @@
+module github.com/liquidata-inc/liquidata-etl-jobs/airflow_dags/nvd_cve
+
+require github.com/aws/aws-sdk-go v1.28.10
+
+go 1.13
---
+++ airflow_dags/nvd_cve/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -eo pipefail
+workdir=$(pwd)
+root=$(dirname "$0")
+cd $root
+ulimit -n 16384
+go run . "$workdir"/index_summary.psv
+cd "$workdir"
+dolt clone Liquidata/common-crawl-index-stats
+cd common-crawl-index-stats
+dolt table import -r entry_stats ../index_summary.psv
+dolt add .
+dolt commit -m 'Importing new data.'
+dolt push origin master:master
