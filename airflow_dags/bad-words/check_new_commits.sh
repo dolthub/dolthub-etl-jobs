6e5e83393d0f016a045b0199616b5aa219f52545
Katie McCulloch <katie@liquidata.co>
2020-04-21 16:39:29 -0700

    airflow_dags/bad-words/check_new_commits.sh: Add if/fi block to check if there is a new PR #130

--- airflow_dags/bad-words/check_new_commits.sh
+++ airflow_dags/bad-words/check_new_commits.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -eo pipefail
-expected="62ed2560e55a8b308f02cf42dee765a22c855927"
-if ! git ls-remote https://github.com/LDNOOBW/List-of-Dirty-Naughty-Obscene-and-Otherwise-Bad-Words HEAD | grep "$expected" > /dev/null; then
+expected_master_hash="62ed2560e55a8b308f02cf42dee765a22c855927"
+expefted_pr_ref="refs/pull/130"
+# Check if there is a new commit on the tip of master
+if ! git ls-remote https://github.com/LDNOOBW/List-of-Dirty-Naughty-Obscene-and-Otherwise-Bad-Words HEAD | grep "$expected_master_hash" > /dev/null; then
+    exit 1
+fi
+# Check if there is a new PR
+if ! git ls-remote https://github.com/LDNOOBW/List-of-Dirty-Naughty-Obscene-and-Otherwise-Bad-Words | grep "$expected_pr_ref" > /dev/null; then
     exit 1
 fi
