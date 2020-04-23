b559dc9f35ae241d1c5b720a02868eaa4ee5c248
Andy Arthur <andy@liquidata.co>
2020-04-22 17:24:04 -0700

    working

---
+++ airflow_dags/nvd/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -eo pipefail
+
+go run .
+dolt clone Liquidata/NVD
+mv *.csv NVD/
+cd NVD
+dolt table import -r CVE CVE.csv
+dolt table import -r CVSS2 CVSS2.csv
+dolt table import -r CVSS3 CVSS3.csv
+dolt table import -r references references.csv
+dolt table import -r reference_tags reference_tags.csv
+dolt table import -r affected_products affected_products.csv
+
