a2eb25e2a14b3b74bf61719d8ca575927373a22f
Dustin Brown <dustin@liquidata.co>
2020-08-17 14:44:06 -0700

    /adhoc/fbi-nibrs/py/import/main.py: Fix import script

--- adhoc/fbi-nibrs/py/import/main.py
+++ adhoc/fbi-nibrs/py/import/main.py
@@ -1,62 +1,63 @@
 import os
+import argparse
 
 states = {
-  # "alabama": "AL",
-  # "alaska": "AK",
-  # "arizona": "AZ",
-  # "arkansas": "AR",
-  # "california": "CA",
-  # "colorado": "CO",
-#   "connecticut": "CT",
-  # "district_of_columbia": "DC",
-  # "delaware": "DE",
-  # "florida": "FL",
+  "alabama": "AL",
+  "alaska": "AK",
+  "arizona": "AZ",
+  "arkansas": "AR",
+  "california": "CA",
+  "colorado": "CO",
+  "connecticut": "CT",
+  "district_of_columbia": "DC",
+  "delaware": "DE",
+  "florida": "FL",
   "georgia": "GA",
-  # "hawaii": "HI",
-  # "idaho": "ID",
-  # "illinois": "IL",
-  # "indiana": "IN",
-  # "iowa": "IA",
-  # "kansas": "KS",
-  # "kentucky": "KY",
-  # "louisiana": "LA",
-  # "maine": "ME",
-  # "maryland": "MD",
-  # "massachusetts": "MA",
-  # "michigan": "MI",
-  # "minnesota": "MN",
-  # "mississippi": "MS",
-  # "missouri": "MO",
-  # "montana": "MT",
-  # "nebraska": "NE",
-  # "nevada": "NV",
-  # "new_hampshire": "NH",
-  # "new_jersey": "NJ",
-  # "new_mexico": "NM",
-  # "new_york": "NY",
-  # "north_carolina": "NC",
-  # "north_dakota": "ND",
-  # "ohio": "OH",
-  # "oklahoma": "OK",
-  # "oregon": "OR",
-  # "pennsylvania": "PA",
-  # "rhode_island": "RI",
-  # "south_carolina": "SC",
-  # "south_dakota": "SD",
-  # "tennessee": "TN",
-  # "texas": "TX",
-  # "utah": "UT",
-  # "vermont": "VT",
-  # "virginia": "VA",
-  # "washington": "WA",
-  # "west_virginia": "WV",
-  # "wisconsin": "WI",
-  # "wyoming": "WY",
+  "hawaii": "HI",
+  "idaho": "ID",
+  "illinois": "IL",
+  "indiana": "IN",
+  "iowa": "IA",
+  "kansas": "KS",
+  "kentucky": "KY",
+  "louisiana": "LA",
+  "maine": "ME",
+  "maryland": "MD",
+  "massachusetts": "MA",
+  "michigan": "MI",
+  "minnesota": "MN",
+  "mississippi": "MS",
+  "missouri": "MO",
+  "montana": "MT",
+  "nebraska": "NE",
+  "nevada": "NV",
+  "new_hampshire": "NH",
+  "new_jersey": "NJ",
+  "new_mexico": "NM",
+  "new_york": "NY",
+  "north_carolina": "NC",
+  "north_dakota": "ND",
+  "ohio": "OH",
+  "oklahoma": "OK",
+  "oregon": "OR",
+  "pennsylvania": "PA",
+  "rhode_island": "RI",
+  "south_carolina": "SC",
+  "south_dakota": "SD",
+  "tennessee": "TN",
+  "texas": "TX",
+  "utah": "UT",
+  "vermont": "VT",
+  "virginia": "VA",
+  "washington": "WA",
+  "west_virginia": "WV",
+  "wisconsin": "WI",
+  "wyoming": "WY",
 }
 
 tableFiles = {
     "CDE_AGENCIES.csv": "cde_agencies",
-#     "AGENCIES.csv": "agencies",
+    "AGENCIES.csv": "agencies",
     "AGENCY_PARTICIPATION.csv": "agency_participation",
     "NIBRS_MONTH.csv": "nibrs_month",
     "NIBRS_INCIDENT.csv": "nibrs_incident",
@@ -78,33 +79,44 @@ tableFiles = {
     "NIBRS_VICTIM_CIRCUMSTANCES.csv": "nibrs_victim_circumstances",
 }
 
-doltRepo="/home/ubuntu/fbi-nibrs"
-
 if __name__ == "__main__":
-    os.chdir(doltRepo)
-    # status = os.popen('dolt status')
-    # print(status.read())
-    print('#!/bin/bash')
-    print('set -eo pipefail')
-    for tableFile in tableFiles:
-        tableName = tableFiles[tableFile]
+    parser = argparse.ArgumentParser()
+    parser.add_argument('importScriptPath', metavar='importScriptPath', type=str, nargs='+',help='path where import scripts should be written')
+    parser.add_argument('dataPath', metavar='dataPath', type=str, nargs='+', help='path to downloaded nibrs data')
+    args = parser.parse_args()
+    importDir = args.importScriptPath[0]
+    dataPath = args.dataPath[0]
+
+    legacyDir = os.path.join(importDir, "legacy")
+    os.makedirs(legacyDir)
+    latestDir = os.path.join(importDir, "latest")
+    os.makedirs(latestDir)
+
+    dirs = {"legacy": legacyDir, "latest": latestDir}
+
+    for key in dirs:
+        writeDir = dirs[key]
         for state in states:
-            abbr = states[state]
-            for year in range(1991, 2019):
+            stateFile = os.path.join(writeDir, "import-{0}.sh".format(state))
+            f = open(stateFile, "a")
+            f.write('#!/bin/bash\n')
+            f.write('set -eo pipefail\n')
 
-                csv_file = "/home/ubuntu/nibrs-data/{0}/{1}/{2}/transformed/{3}".format(state, year, abbr, tableFile)
-                if not os.path.exists(csv_file):
+            for tableFile in tableFiles:
+                tableName = tableFiles[tableFile]
+                abbr = states[state]
+                if key == "legacy" and tableName == "agencies":
                     continue
-                # try:
-#                     print("importing data for state: {0} year: {1} table: {2}".format(state, year, tableFile))
-#                     update_table = os.popen(
-#                         'dolt table import -u {} {}'.format(tableName, csv_file))
-#                     print(update_table.read())
-                    # add_table = os.popen('dolt add -A')
-                print('echo \"{0}\"'.format(csv_file))
-                print('dolt table import -u {} {}'.format(tableName, csv_file))
-#     print('dolt add .')
-                # except OSError as e:
-                #     print("error importing data for state: {0} year: {1} table: {2} error: {3}".format(state, year, tableFile, e))
+                if key == "latest" and (tableName == "cde_agencies" or tableName == "agency_participation"):
+                    continue
 
+                for year in range(1991, 2019):
+                    csv_file = "{0}/{1}/{2}/{3}/transformed/{4}".format(dataPath, state, year, abbr, tableFile)
+                    if not os.path.exists(csv_file):
+                        continue
 
+                    f.write('echo \"{0}\"\n'.format(csv_file))
+                    f.write('dolt table import -u {} {}\n'.format(tableName, csv_file))
+                    f.write('\n')
+
+            f.close()
